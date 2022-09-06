4e8633e1c2d94aa371f5cd1984d21c4b333315ac
unknown <sathishfreelanc5@gmail.com>
2022-09-06 18:00:04 +0530

    restdjangoapi-task

---
+++ backendapp/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+          model = Todo
+          fields =('id','title','description','completed')
